27799719b0ae940eb0490ed68578c4274a8abb87
Suman-ojha <sumanojha1999@gmail.com>
2024-07-18 19:09:30 +0530

    subscription modules added

---
+++ blog_frontend/src/Components/SubscriptionCards.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { HiArrowRightCircle } from "react-icons/hi2";
+
+const SubscriptionCards = ({ data }) => {
+    return (
+        <div className='w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
+            <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">{data.title}</h5>
+            <div class=" items-baseline text-gray-900 dark:text-white">
+                <span class="text-3xl font-semibold">₹</span>
+                <span class="text-3xl font-extrabold tracking-tight">{data.price}</span>
+                <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">/month</span>
+            </div>
+            {/* role_slug_name: "ai_chat",
+          role_name: "AI Blog writter" */}
+            <ul role="list" class="space-y-5 my-7">
+                {data && data.role && (
+                    data.role.map((item, idx) => (
+                        <li class="flex items-center">
+                            <HiArrowRightCircle size='30px' />
+                            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">{item.role_name}</span>
+                        </li>
+                    ))
+                )}
+
+            </ul>
+            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choose plan</button>
+        </div>
+    )
+}
+
+export default SubscriptionCards
--- blog_frontend/src/pages/Home.jsx
+++ blog_frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import CallToAction from '../Components/CallToAction';
 import { useEffect, useState } from 'react';
 import PostCard from '../Components/PostCard';
+import SubscriptionCards from '../Components/SubscriptionCards';
 
 const Home = () => {
   const [posts, setPosts] = useState([])
@@ -31,7 +32,81 @@ const Home = () => {
       isMounted = false;
     }
   }, [])
-  console.log(posts, "<<post");
+  // console.log(posts, "<<post");
+  const subscribe_data = [
+    {
+      id: 1,
+      title: "Normal Plan",
+      price: 199,
+      currency: 'INR',
+      role: [
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "RollBase access"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "Unlimited file action"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "Admin dashboard access"
+        },
+      ]
+    },
+    {
+      id: 2,
+      title: "Gold Plan",
+      price: 299,
+      currency: 'INR',
+      role: [
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+      ]
+    },
+    {
+      id: 3,
+      title: "Pro Plan",
+      price: 499,
+      currency: 'INR',
+      role: [
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+        {
+          role_slug_name: "ai_chat",
+          role_name: "AI Blog writter"
+        },
+      ]
+    },
+  ]
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
@@ -49,6 +124,14 @@ const Home = () => {
       </div>
       <div className='p-3 bg-amber-100 dark:bg-slate-700'>
         <CallToAction />
+      </div>
+      <div className="flex flex-col  items-center mb-5">
+        <h1 className='text-xl mt-5 font-semibold'>Subscription</h1>
+        <div className="mt-5 flex flex-wrap gap-5 justify-center md:flex-nowrap">
+          {
+            subscribe_data && subscribe_data.map((item, idx) => <SubscriptionCards key={idx} data={item} />)
+          }
+        </div>
       </div>
       <div className='flex flex-col  items-center mb-5'>
         <h1 className='text-xl mt-5 font-semibold'>Recent Posts</h1>
